c5ad3009994c198678cce1296d63582d56b37036
therealjustindude <therealjustindude@gmail.com>
2020-11-07 14:51:35 -0600

    composewithdevtools and root reducer imported

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers/index'
 import thunk from 'redux-thunk';
 
 import App from './App';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
 	<Provider store={store}>
