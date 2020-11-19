8a5f35cce47362d8024be325df9708ba847391ba
therealjustindude <therealjustindude@gmail.com>
2020-11-19 09:04:24 -0600

    app wrapped in router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {BrowserRouter as Router} from 'react-router-dom' 
 import rootReducer from './reducers/indexReducer'
 import thunk from 'redux-thunk';
 import App from './App';
@@ -11,7 +12,9 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 
 ReactDOM.render(
 	<Provider store={store}>
-   		<App />
+		<Router>
+			<App />
+		</Router>
 	</Provider>
 	, document.getElementById('root'));
 
