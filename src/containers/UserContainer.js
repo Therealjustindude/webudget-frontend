1ec28f661fdd3d0e8b14c0c69481fdbdf7e79771
therealjustindude <therealjustindude@gmail.com>
2020-11-22 16:58:25 -0600

    navbar and log out button added

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
 import ExpensesTable from '../components/ExpensesTable'
+import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components'
+
 
 class UserContainer extends Component {
-	
-	
+	handleLogOut = () => {
+		localStorage.removeItem('currentUser')
+		localStorage.removeItem('authToken')
+	}
+	// add: expenses form to be used for create and update
 	render() {
 		return (
-			<ExpensesTable />
+			<>
+				<Paper style={{overflow:'hidden', alignItems: 'center', padding:'5px', margin:'5px' }}>
+					<Styledh1>weBudget</Styledh1>
+					<Button onClick={this.handleLogOut} variant="outlined" style={buttonStyle} href="/">
+  						Log Out
+					</Button>
+				</Paper>
+				<ExpensesTable />
+			</>
 		)
 	}
 }
 
+const buttonStyle = {
+	display: "flex",
+	float: "right",
+	margin: "10px"
+}
+const Styledh1 = styled.h1`
+  font-size: 2.5em;
+  font-weight: bold;
+  float: left;
+  margin: 5px;
+`;
 
 export default UserContainer
