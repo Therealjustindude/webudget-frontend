e6a9217f5b1ee56f93e94642b3a3e47cc4750275
therealjustindude <therealjustindude@gmail.com>
2020-11-23 13:08:06 -0600

    removed expenses.js renamed to userexpenses

--- src/actions/expenses.js
+++
@@ -1,9 +0,0 @@
-export const getExpenses = () => {
-	return ((dispatch) => {
-		dispatch({ type: "LOADING_EXPENSES" })
-		return fetch('http://localhost:3001/api/v1/users/:id/expenses')
-			.then(res => res.json())
-			.then(expenses => dispatch({ type: "EXPENSES_LOADED", payload: expenses }))
-			.then(expenses => console.log(expenses))
-	})
-}
