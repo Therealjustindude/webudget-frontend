a2f8a56aade1d19263518ce9c2effeca3d6ebeb1
therealjustindude <therealjustindude@gmail.com>
2020-11-23 13:07:43 -0600

    add/edit expense form added to routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import './index.css';
 const LoginForm = lazy(() => import('./components/LoginForm'));
 const SignupForm = lazy(() => import('./components/SignupForm'));
 const UserContainer = lazy(() => import('./containers/UserContainer'));
+const AddExpenseForm = lazy(() => import('./components/AddExpenseForm'));
+const EditExpenseForm = lazy(() => import('./components/EditExpenseForm'));
 
 class App extends Component {
   
@@ -18,8 +20,10 @@ class App extends Component {
         <Suspense fallback={<h1>Loading...</h1>}>
           <Switch>
             <Route exact path="/" render={(routerProps) => <LoginForm history={routerProps.history}/>} />
-            <Route path='/signup' render={(routerProps) => <SignupForm history={routerProps.history} />} />
-            <Route path='/users/:id' render={(routerProps) => <UserContainer history={routerProps.history} />} />
+            <Route exact path='/signup' render={(routerProps) => <SignupForm history={routerProps.history} />} />
+            <Route exact path='/users/:id' render={(routerProps) => <UserContainer history={routerProps.history} />} />
+            <Route exact path='/users/:id/expenses/add' render={(routerProps) => <AddExpenseForm history={routerProps.history} />} />
+            <Route exact path='/users/:id/expenses/:id/update' render={(routerProps) => <EditExpenseForm history={routerProps.history} />} />
           </Switch>
         </Suspense>
       </div>
