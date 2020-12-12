1286286e3570f96cae450ecb8aaa8077678f02af
therealjustindude <therealjustindude@gmail.com>
2020-12-12 14:34:36 -0600

    changed from class to functional

--- src/containers/UserTableContainer.js
+++ src/containers/UserTableContainer.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react'
 import ExpensesTable from '../components/expense/ExpensesTable'
 import DebtTable from '../components/debt/DebtTable'
 
-export default class UserTableContainer extends Component {
-	render() {
+export const UserTableContainer = (currentUser) => {
 		return (
 			<>
-				<ExpensesTable currentUser={this.props.currentUser.id} userExpenses={this.props.currentUser.expenses} history={this.props.history} />
-				<DebtTable currentUser={this.props.currentUser.id} userDebts={this.props.currentUser.debts} history={this.props.history}/>
+				<ExpensesTable currentUser={currentUser.currentUser.id} userExpenses={currentUser.currentUser.expenses}  />
+				<DebtTable currentUser={currentUser.currentUser.id} userDebts={currentUser.currentUser.debts} />
 			</>
 		)
-	}
 }
