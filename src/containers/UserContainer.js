871e3c55351d65e80061b8ea6c8a0f81af89ebaf
therealjustindude <therealjustindude@gmail.com>
2020-12-12 14:35:46 -0600

    local state removed and header and footer added

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,61 +1,32 @@
 import React, { Component } from 'react'
-import { removeAuth } from '../actions/localStorage'
-import Button from '@material-ui/core/Button';
-// import ExpensesTable from '../components/ExpensesTable'
-// import DebtTable from '../components/DebtTable'
-import Paper from '@material-ui/core/Paper';
-import styled from 'styled-components'
+
 import { connect } from 'react-redux'
 import {  saveState } from '../actions/localStorage'
-import  UserTableContainer  from './UserTableContainer';
+import { UserTableContainer }  from './UserTableContainer';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 
 
 class UserContainer extends Component {
-	state ={
-		user: this.props.user ? this.props.user : {}
-	}
-
-	handleLogOut = () => {
-		removeAuth()
-	}
-
 	componentDidMount() {
 		if (localStorage.getItem("currentUser") === null) {
+			debugger
 			saveState(this.props.user)
 		}
 	}
-	
 	render() {
 		return (
 			<>
-				<Paper style={{overflow:'hidden', alignItems: 'center', padding:'5px', margin:'5px', display: 'flex',justifyContent: 'space-between' }}>
-					<Styledh1>weBudget</Styledh1>
-					<StyledP>{new Date().toDateString()}</StyledP>
-					<Button onClick={this.handleLogOut} variant="outlined" style={buttonStyle} href="/">
-  						Log Out
-					</Button>
-				</Paper>
-				<UserTableContainer currentUser={this.state.user} />
+				<Header />
+				<UserTableContainer currentUser={this.props.user} />
+				<Footer/>
 			</>
 		)
 	}
 }
 
-const buttonStyle = {
-	display: "flex",
-	float: "right",
-	margin: "10px"
-}
-const Styledh1 = styled.h1`
-  font-size: 2.5em;
-  font-weight: bold;
-  float: left;
-  margin: 5px;
-`;
-const StyledP = styled.span`
-  font-size: 0.9em;
-`;
+
 
 const mSTP = (state) => {
 	return {
