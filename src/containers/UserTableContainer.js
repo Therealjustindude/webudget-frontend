7b12324cecf346a7de3be53ce2951ca5325c4cb5
therealjustindude <therealjustindude@gmail.com>
2021-03-04 08:47:22 -0600

    commented out debts table for now

--- src/containers/UserTableContainer.js
+++ src/containers/UserTableContainer.js
@@ -1,11 +1,11 @@
 import ExpensesTable from '../components/expense/ExpensesTable'
-import DebtTable from '../components/debt/DebtTable'
+// import DebtTable from '../components/debt/DebtTable'
 
 export const UserTableContainer = (currentUser) => {
 		return (
 			<>
 				<ExpensesTable currentUser={currentUser.currentUser.id} userExpenses={currentUser.currentUser.expenses}  />
-				<DebtTable currentUser={currentUser.currentUser.id} userDebts={currentUser.currentUser.debts} />
+				{/* <DebtTable currentUser={currentUser.currentUser.id} userDebts={currentUser.currentUser.debts} /> */}
 			</>
 		)
 }
